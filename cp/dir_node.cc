27b5f0362836f67ab06bfe3268a4e9af6bf8a2a7
IIllIlllIl <wang.tr@outlook.com>
2021-09-14 01:03:51 +0800

    1.1-dir

---
+++ cp/dir_node.cc
@@ -0,0 +1,5 @@
+//
+// Created by Phoenix Wang on 2021/9/13.
+//
+
+#include "dir_node.h"
---
+++ cp/dir_node.h
@@ -0,0 +1,14 @@
+//
+// Created by Phoenix Wang on 2021/9/13.
+//
+
+#ifndef ZNFS_DIR_NODE_H
+#define ZNFS_DIR_NODE_H
+
+
+class dir_node {
+
+};
+
+
+#endif //ZNFS_DIR_NODE_H
