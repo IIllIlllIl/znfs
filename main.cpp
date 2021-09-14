c77175185c96212d3ac97b40fa8c024eca530f4c
IIllIlllIl <wang.tr@outlook.com>
2021-09-14 09:23:20 +0800

    1.3-error:stat

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@
 using namespace std;
 
 int main() {
-    string root = "../test/dir/";
+    string root = "../test/dir/dir2/";
     string dest = root + "cp_";
-    string file = "hs";
+    string file = "fifo";
     dir_node dn((root + file).c_str(), (dest + file).c_str());
     copy_tool cp((root + file).c_str(), (dest + file).c_str());
     cout<<cp.copy()<<endl;
